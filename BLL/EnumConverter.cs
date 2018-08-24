d4f43888b8aa0b716396d5ded0031ee436f6388c
OJT <jglbulacan1@gmail.com>
2018-08-24 10:25:44 +0800

    test

---
+++ BLL/EnumConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace VRegistration.BLL
+{
+    public class EnumConverter : IValueConverter
+    {
+        public object Convert(object val, Type target, object param, System.Globalization.CultureInfo culture)
+        {
+            return val.Equals(param);
+        }
+
+        public object ConvertBack(object val, Type target, object param, System.Globalization.CultureInfo culture)
+        {
+            return val.Equals(true) ? param : Binding.DoNothing;
+        }
+    }
+    //nanidafaq
+}
