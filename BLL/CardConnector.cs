9fa4853491bb8de5dee9c8b44e9f940fa9d9c9c3
OJT <jglbulacan1@gmail.com>
2018-08-29 13:41:30 +0800

    test

--- BLL/CardConnector.cs
+++ BLL/CardConnector.cs
@@ -294,15 +294,15 @@ namespace VRegistration.BLL
             }
             else
             {
-                cardUID = BitConverter.ToString(receivedUID.Take(5).ToArray()).ToLower();
+                cardUID = BitConverter.ToString(receivedUID.Take(4).ToArray()).ToLower();
                 //remove dashes
-                cardUID = BitConverter.ToString(receivedUID.Take(5).Reverse().ToArray()).Replace("-", string.Empty).ToLower();
+                cardUID = BitConverter.ToString(receivedUID.Take(4).ToArray()).Replace("-", string.Empty).ToLower();
 
                 //get in reverse
                 //cardUID = BitConverter.ToString(receivedUID.Take(4).ToArray()).Replace("-", string.Empty).ToLower();
             }
 
-            return new string (cardUID.Reverse().ToArray());
+            return cardUID;
         }
         public void SelectDevice()
         {
